a55ec344baccdbb314e9fcf7d47dbb6e2a57aa0a
Mahnoor Fatima <mahnoor.fatima132@gmail.com>
2024-03-17 00:24:56 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"], // Specify the external dependencies here
-    },
+  optimizeDeps: {
+    exclude: ["react-router-dom"], // Exclude react-router-dom from optimization
   },
 });
