75c91e2fde53877ee1a0a2f6d20e3b6115dbad79
Mahnoor Fatima <mahnoor.fatima132@gmail.com>
2024-03-17 00:29:06 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["react-router-dom", "react-just-parallax", "scroll-lock"], // Exclude these dependencies from optimization
+  build: {
+    rollupOptions: {
+      external: ["react-router-dom", "scroll-lock"], // Add 'scroll-lock' to external dependencies
+    },
   },
 });
